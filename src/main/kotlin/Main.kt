e3cb2898e100cfa1c18ae527869e7c0bcbaae1b6
jjun012 <duwns2057@naver.com>
2026-07-15 11:54:07 +0900

    3,4단계

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,18 +6,21 @@ fun main() {
     val sc = Scanner(System.`in`)
     println("== 명언 앱 ==")
 
+    var num = 0
+
     while (true) {
         print("명령) ")
-        val command = sc.nextLine()
+        when (sc.nextLine()) {
+            "종료" -> break
+            "등록" -> {
+                print("명언 : ")
+                val content = sc.nextLine()
+                print("작가 : ")
+                val author = sc.nextLine()
 
-        if (command =="종료") {
-            break
-        }else if (command=="등록") {
-            print("명언 : ")
-            val content = sc.nextLine()
-            print("작가 : ")
-            val author = sc.nextLine()
-
+                num++
+                println("${num}번 명언이 등록되었습니다.")
+            }
         }
     }
 }
