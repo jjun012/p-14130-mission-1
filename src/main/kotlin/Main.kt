0c29439b3a46b4a792efcf8ef981995abf3cf958
jjun012 <duwns2057@naver.com>
2026-07-15 14:45:16 +0900

    6단계

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,19 +2,23 @@ package com
 
 import java.util.Scanner
 
-data class Quote(val num: Int,val content: String, val author: String)
+data class Quote(val num: Int, val content: String, val author: String)
+
 fun main() {
     val sc = Scanner(System.`in`)
     println("== 명언 앱 ==")
 
-    val list=mutableListOf<Quote>()
+    val list = mutableListOf<Quote>()
     var num = 0
 
     while (true) {
         print("명령) ")
-        when (sc.nextLine()) {
-            "종료" -> break
-            "등록" -> {
+        val command = sc.nextLine()
+
+        when {
+            command == "종료" -> break
+
+            command == "등록" -> {
                 print("명언 : ")
                 val content = sc.nextLine()
                 print("작가 : ")
@@ -24,13 +28,20 @@ fun main() {
                 println("${num}번 명언이 등록되었습니다.")
                 list.add(Quote(num, content, author))
             }
-            "목록" -> {
+
+            command == "목록" -> {
                 println("번호 / 작가 / 명언")
                 println("----------------------")
                 for (i in list.size - 1 downTo 0) {
                     val quote = list[i]
                     println("${quote.num} / ${quote.author} / ${quote.content}")
                 }
+            }
+
+            command.startsWith("삭제?id=") -> {
+                val id = command.substringAfter("id=").toInt()
+                list.removeIf { it.num == id }
+                println("${id}번 명언이 삭제되었습니다.")
             }
         }
     }
