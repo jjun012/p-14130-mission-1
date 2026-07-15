4ec1609cc82f10c8745fb729eec704e14307f48f
jjun012 <duwns2057@naver.com>
2026-07-15 11:32:44 +0900

    2단계

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,23 @@
 package com
 
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+import java.util.Scanner
+
 fun main() {
-    println("== 명언 앱 ==");
-    println("명령) 종료");
+    val sc = Scanner(System.`in`)
+    println("== 명언 앱 ==")
+
+    while (true) {
+        print("명령) ")
+        val command = sc.nextLine()
+
+        if (command =="종료") {
+            break
+        }else if (command=="등록") {
+            print("명언 : ")
+            val content = sc.nextLine()
+            print("작가 : ")
+            val author = sc.nextLine()
+
+        }
+    }
 }
